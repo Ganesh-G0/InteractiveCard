35bd841266d5f8305df0aad647ff4bd1a412c732
Ganesh <121486938+Ganesh-G0@users.noreply.github.com>
2023-12-09 21:28:03 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,36 @@
+addEventListener("input", () => {      
+  $(".card-name").text($("#name").val());
+  $(".card-num").text(newCardNumber($("#cardNumber").val()));
+  $(".card-month").text($("#exp-month").val());
+  $(".card-year").text($("#exp-year").val());
+  $(".cvc-num").text($("#cvc").val());
+});
+
+$("#button2").click(()=> {
+  $("#form").removeClass("d-none");
+  $("#complete").addClass("d-none");
+});
+
+
+function newCardNumber(cardNumber) {
+    let card = "";
+    for (let i = 0; i < cardNumber.length; i++) {
+        if (i % 4 == 0 && i > 0) {
+            card = card.concat(" ");
+        }
+        card = card.concat(cardNumber[i]);
+    }
+    return card;
+}
+
+addEventListener('click', ()=> {
+  if ($('#cvc').val() && $('#exp-year').val() && 
+  $('#exp-month').val() && $('#cardNumber').val() 
+  && $('#name').val()
+   != '') {
+    $("#button").click(()=> {
+      $("#form").addClass("d-none");
+      $("#complete").removeClass("d-none");    
+    });    
+  }  
+})
